6a204b70bdebfbdd168bb2632fa485d98f7a23ca
Neo <huhaohao525@sina.com>
2025-08-12 16:37:53 +0800

    refactor: remove myrdkit-makemol

--- examples/synthemol/features/featurization.py
+++ examples/synthemol/features/featurization.py
@@ -4,7 +4,24 @@ from itertools import zip_longest
 import logging
 from rdkit import Chem
 import numpy as np
-from myrdkit.myrdkit import make_mol
+
+def make_mol(s: str, keep_h: bool, add_h: bool):
+    """
+    Builds an RDKit molecule from a SMILES string.
+    
+    :param s: SMILES string.
+    :param keep_h: Boolean whether to keep hydrogens in the input smiles. This does not add hydrogens, it only keeps them if they are specified.
+    :return: RDKit molecule.
+    """
+    if keep_h:
+        mol = Chem.MolFromSmiles(s, sanitize=False)
+        Chem.SanitizeMol(mol, sanitizeOps=Chem.SanitizeFlags.SANITIZE_ALL ^
+            Chem.SanitizeFlags.SANITIZE_ADJUSTHS)
+    else:
+        mol = Chem.MolFromSmiles(s)
+    if add_h:
+        mol = Chem.AddHs(mol)
+    return mol
 
 
 class Featurization_parameters:
